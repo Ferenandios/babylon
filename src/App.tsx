91cc165528a286f96f3f44c082fdfddd3151bb55
Ferenandios <www.terunplay@gmail.com>
2024-02-06 09:23:43 +0300

    Added Header-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { FC } from "react";
+import Header from "./components/Header/Header";
 
 const App: FC = (): JSX.Element => {
-  return <div>works!</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
